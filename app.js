978c8164b39b244fe4280a1108f7b7cd96557404
kydallaboutlearning <kydallaboutlearning@gmail.com>
2025-01-14 18:24:55 -0800

    git commit -m "Refact(JS): Added the js code to the hero section
    Added the functionality to clone the text in javascripts

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const menu = document.querySelector("#mobile-menu");
-const menulinks = document.querySelector('.navbar__menu');
+const menulinks = document.querySelector(".navbar__menu");
 
-menu.addEventListener("click", function(){
-    menu.classList.toggle('is-active');
-    menulinks.classList.toggle('active');
+menu.addEventListener("click", function () {
+  menu.classList.toggle("is-active");
+  menulinks.classList.toggle("active");
 });
+
+// Adding javascrippt to clone the logo
+const logos = document.querySelector(".rotating-logos__track").cloneNode(true);
+document.querySelector(".rotating-logos__container").appendChild(logos);
